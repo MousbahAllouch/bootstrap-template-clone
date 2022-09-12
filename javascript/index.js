f5058989c8a90858e57aad27bd3d71880ad8f803
MOUSBAH <alloushmousbah1999@gmail.com>
2022-09-12 21:20:58 +0300

    check-fullname-input

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,11 @@
 
+
 let navButton1= document.getElementById("navbar-button1");
 let navButton2= document.getElementById("navbar-button2");
 let navButton3= document.getElementById("navbar-button3");
 
+
+// here if the user go over the button it's color changed to #1abc9c
 navButton1.onmouseover = function(){
     navButton1.style.color="#1abc9c"
 };
@@ -12,3 +15,16 @@ navButton2.onmouseover = function(){
 navButton3.onmouseover = function(){
     navButton3.style.color="#1abc9c"
 };
+
+function submitting(){
+    let fullName=document.getElementById("full-name").value
+    let email=document.getElementById("email").value
+    let phoneNumber=document.getElementById("phone-number").value
+    let message=document.getElementById("message").value
+
+    // to check the lenght of fullName
+    if(fullName.length>5){
+        console.log(fullName)
+    }
+    
+}
